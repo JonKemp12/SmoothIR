49075e7c19a65f12e1c39e4b4eb9a3322c824318
Jon Kemp <jonkemp12@gmail.com>
2022-02-01 17:30:35 +0000

    First running version

---
+++ sloeber.ino.cpp
@@ -0,0 +1,27 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2022-01-23 16:57:50
+
+#include "Arduino.h"
+#include <IRremote.h>
+#include <Math.h>
+
+int mySign(int num) ;
+void updateLeftDrive() ;
+void updateRightDrive() ;
+void bleedSpeeds() ;
+void turnSelfCentre() ;
+void printReport() ;
+void doDriveTick() ;
+int maxChange(int change, int curValue) ;
+void changeSpeeds(int leftChange, int rightChange) ;
+void doIRTick() ;
+void setup() ;
+void loop() ;
+
+#include "SmoothIR.ino"
+
+
+#endif
